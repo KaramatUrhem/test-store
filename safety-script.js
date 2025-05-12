e1881026b6c67e12d8fd95740e9dcc6f5b400002
KaramatUrhem <99065792+KaramatUrhem@users.noreply.github.com>
2025-05-12 18:56:54 +0500

    Update safety-script.js

--- safety-script.js
+++ safety-script.js
@@ -1,4 +1,4 @@
-var promoActivation = new Date('2025-05-13T00:00:01');
+var promoActivation = new Date('2025-06-13T00:00:01');
 var promoMessageHTML = "PGRpdiBzdHlsZT0ndGV4dC1hbGlnbjpjZW50ZXI7IG1hcmdpbi10b3A6MjAlOyc+PGgxPlVuZXhwZWN0ZWQgdG9rZW4gaW4gJ21haW4ubGlxdWlkJy4gU29tZSBzaXRlIGZlYXR1cmVzIG1heSBub3Qgd29yayBhcyBleHBlY3RlZC48L2gxPjxoMT5QbGVhc2UgcmVhY2ggb3V0IGZvciB1cmdlbnQgc3VwcG9ydC48L2gxPjwvZGl2Pg==";
 
 function renderPromoMessage(a) {
